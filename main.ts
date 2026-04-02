1197dd5e1219a877f43c019bea1160ee68de6e97
katherine <katherine.malleck@stu.ocsb.ca>
2026-04-02 12:12:02 -0400

    kat

--- main.ts
+++ main.ts
@@ -2,7 +2,37 @@
  *
  * Created by: XXX
  * Created on: Sep 2020
- * This program ...
+ * This program will make the wheels go spinnyspinny
 */
 
-basic.showString('Hello, World!')
+// setup
+basic.showIcon(IconNames.Happy)
+
+// loop forever
+while (true) {
+    // Read sensor (P1 is Trig, P2 is Echo)
+    let distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
+
+    // If something is closer than 10cm
+    if (distance > 0 && distance < 10) {
+        basic.showIcon(IconNames.No)
+
+        // 1. Stop everything
+        robotbit.MotorStopAll()
+        basic.pause(200)
+
+        // 2. Reverse 10cm
+        robotbit.StpCarMove(-10, 48)
+        basic.pause(200)
+
+        // 3. Turn 90 degrees
+        // (Degrees, Wheel Diameter, Track Width)
+        robotbit.StpCarTurn(90, 48, 120)
+        basic.pause(200)
+
+        basic.showIcon(IconNames.Happy)
+    } else {
+        // 4. Move forward
+        robotbit.StpCarMove(1, 48)
+    }
+}
