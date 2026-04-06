6a2f0a20e2a9d19de19ec51e801ac24760194bf2
katherine <katherine.malleck@stu.ocsb.ca>
2026-04-06 19:51:12 -0400

    broken

--- main.ts
+++ main.ts
@@ -1,38 +1,50 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
- * Created by: XXX
- * Created on: Sep 2020
+ * Created by: katherine
+ * Created on: Apr 2026
  * This program will make the wheels go spinnyspinny
 */
 
-// setup
-basic.showIcon(IconNames.Happy)
+let started = false
+basic.showIcon(IconNames.Asleep)
 
-// loop forever
+input.onButtonPressed(Button.A, function () {
+    started = true
+    basic.showIcon(IconNames.Happy)
+})
+
 while (true) {
-    // Read sensor (P1 is Trig, P2 is Echo)
-    const distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
 
-    // If something is closer than 10cm
-    if (distance > 0 && distance < 10) {
-        basic.showIcon(IconNames.No)
+    if (started) {
 
-        // 1. Stop everything
-        robotbit.MotorStopAll()
-        basic.pause(200)
+        const distance = sonar.ping(DigitalPin.P1, DigitalPin.P2, PingUnit.Centimeters)
 
-        // 2. Reverse 10cm
-        robotbit.StpCarMove(-10, 48)
-        basic.pause(200)
+        // Object detected
+        if (distance > 0 && distance < 10) {
+            basic.showIcon(IconNames.No)
 
-        // 3. Turn 90 degrees
-        // (Degrees, Wheel Diameter, Track Width)
-        robotbit.StpCarTurn(90, 48, 120)
-        basic.pause(200)
+            // Stop
+            robotbit.MotorStopAll()
+            basic.pause(200)
 
-        basic.showIcon(IconNames.Happy)
-    } else {
-        // 4. Move forward
-        robotbit.StpCarMove(1, 48)
+            // Reverse 10 cm
+            robotbit.StpCarMove(-10, 48)
+            basic.pause(200)
+
+            // Turn approx 90°
+            robotbit.MotorRun(robotbit.Motors.M1A, 80)
+            robotbit.MotorRun(robotbit.Motors.M2A, -80)
+            basic.pause(400)
+
+            robotbit.MotorStopAll()
+            basic.pause(200)
+
+            basic.showIcon(IconNames.Happy)
+        }
+
+        // Keep moving forward in small steps
+        if (distance >= 10 || distance == 0) {
+            robotbit.StpCarMove(1, 48)
+        }
     }
+    basic.pause(50) // tiny delay to let sensor respond
 }
